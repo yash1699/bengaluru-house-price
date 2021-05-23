034cc1a5f1da58e73779e5fb1462d2e17ade8c2a
Yash Verma <vermayash.16@gmail.com>
2021-05-23 15:54:32 +0530

    First Commit

---
+++ server/util.py
@@ -0,0 +1,44 @@
+import json
+import pickle
+import numpy as np
+__locations = None
+__data_columns = None
+__model = None
+
+
+def get_location_names():
+    return __locations
+
+
+def load_saved_artifacts():
+    print("--Loading Saved Artifacts--")
+    global __data_columns
+    global __locations
+    global __model
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+    print("Loading Done.")
+
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)*100000
+
+
+if __name__ == "__main__":
+    load_saved_artifacts()
